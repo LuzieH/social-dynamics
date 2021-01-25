e0e86b5c4614c607d67fb3791ce964628d9a6576
FMalerba <malerbafede@gmail.com>
2021-01-25 15:57:10 +0100

    Added properties

--- social_dynamics/agent_networks/agent_network.py
+++ social_dynamics/agent_networks/agent_network.py
@@ -4,9 +4,17 @@ import numpy as np
 
 class AgentNetwork(ABC):
     
-    def __init__(self, adjecency_matrix: np.ndarray, agents: np.ndarray) -> None:
-        self._adjacency_matrix = adjecency_matrix
+    def __init__(self, adjacency_matrix: np.ndarray, agents: np.ndarray) -> None:
+        self._adjacency_matrix = adjacency_matrix
         self._agents = agents
+    
+    @property
+    def adjacency_matrix(self):
+        return self._adjacency_matrix
+    
+    @property
+    def agents(self):
+        return self._agents
     
     def step(self, **kwargs) -> None:
         self._step(self, **kwargs)
