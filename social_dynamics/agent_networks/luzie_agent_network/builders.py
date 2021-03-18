27e16630cef44166dad4ba16a7868676909a0210
FMalerba <malerbafede@gmail.com>
2021-03-18 12:51:26 +0100

    Moved LuzieAgentNetwork to independent submodule. General reformating and cleanup of the code.

---
+++ social_dynamics/agent_networks/luzie_agent_network/builders.py
@@ -0,0 +1,59 @@
+import gin
+import numpy as np
+from social_dynamics.agent_networks.luzie_agent_network import agent_types
+from typing import Callable, Dict, List
+
+AdjMatrixBuilder = Callable[[int], np.ndarray]
+AgentsBuilder = Callable[[int, int], np.ndarray]
+ParamsBuilder = Callable[..., Dict[str, np.ndarray]]
+
+
+@gin.configurable(module="Luzie")
+def complete_adjacency_matrix_builder(n_agents: int) -> np.ndarray:
+    """Builds an adjacency matrix of ones; corresponding to a complete network structure"""
+    return np.ones(shape=(n_agents, n_agents))
+
+
+@gin.configurable(module="Luzie")
+def random_normal_agent_builder(n_agents: int, n_options: int) -> np.ndarray:
+    """Initializes the states of all the agents using a Normal distribution."""
+    return np.random.normal(size=(n_agents, n_options))
+
+
+@gin.configurable(module="Luzie")
+def homogenous_builder(adjacency_matrix: np.ndarray, n_options: int, alpha: float, beta: float, gamma: float,
+                       delta: float, d: float, u: float, v: float, b: float) -> Dict[str, np.ndarray]:
+    """
+    Sets all the parameters and tensors to update a General Opinion Dynamics-style network.
+    Follows the structure presented for the homogenous case in https://arxiv.org/abs/2009.04332.
+    """
+    # Adjacency Tensor
+    n_agents = adjacency_matrix.shape[0]
+    adjacency_tensor = np.ones(shape=(n_agents, n_agents, n_options, n_options)) * delta
+    option_diag = np.diagonal(adjacency_tensor, axis1=2, axis2=3)
+    option_diag.setflags(write=1)  # np.diagonal return value is read-only view of input array
+    option_diag[:, :, :] = gamma
+    agent_diag = np.diagonal(adjacency_tensor, axis1=0, axis2=1)
+    agent_diag.setflags(write=1)  # np.diagonal return value is read-only view of input array
+    agent_diag[:, :, :] = beta
+    both_diag = np.diagonal(option_diag, axis1=0, axis2=1)
+    both_diag.setflags(write=1)  # np.diagonal return value is read-only view of input array
+    both_diag[:, :] = alpha
+    adjacency_tensor = np.einsum('ijkl,ij->ijkl', adjacency_tensor, adjacency_matrix)
+
+    # Update rule parameters
+    resistance = np.ones(shape=(n_agents, n_options)) * d
+    same_option_attention = np.ones(shape=(n_agents, 1)) * u
+    other_options_attention = np.ones(shape=(n_agents, 1)) * v
+    inputs = np.ones(shape=(n_agents, n_options)) * b
+
+    params = {
+        "adjacency_tensor": adjacency_tensor,
+        "resistance": resistance,
+        "same_option_attention": same_option_attention,
+        "other_options_attention": other_options_attention,
+        "inputs": inputs
+    }
+
+    return params
+
--- social_dynamics/agent_networks/luzie_agent_network/luzie_agent_network.py
+++ social_dynamics/agent_networks/luzie_agent_network/luzie_agent_network.py
@@ -1,22 +1,20 @@
 import numpy as np
 import gin
 from social_dynamics.agent_networks import agent_network
-from social_dynamics.agent_networks.god_agent_network.builders import ActivationFunction, AdjMatrixBuilder, AgentsBuilder, ParamsBuilder
+from social_dynamics.agent_networks.luzie_agent_network.builders import AdjMatrixBuilder, AgentsBuilder, ParamsBuilder
 
 
 @gin.configurable()
-class GODAgentNetwork(agent_network.AgentNetwork):
+class LuzieAgentNetwork(agent_network.AgentNetwork):
 
     def __init__(self,
                  adj_matrix_builder: AdjMatrixBuilder,
                  agents_builder: AgentsBuilder,
                  parameters_builder: ParamsBuilder,
-                 S1: ActivationFunction,
-                 S2: ActivationFunction,
                  time_interval: float = 0.01,
                  noise_std: float = 0) -> None:
         """
-        Implementation of the model presented in https://arxiv.org/abs/2009.04332.
+        Implementation of Luzie's variation of the model presented in https://arxiv.org/abs/2009.04332.
         
         Args:
             adj_matrix_builder: Builds and returns the adjacency to be used by the
@@ -26,8 +24,6 @@ class GODAgentNetwork(agent_network.AgentNetwork):
                         The agents' representation will have shape (n_agents, n_options)
             parameters_builder: Builds all the other parameters required to update the model.
                         Returns a dictionary containing all the necessary parameters.
-            S1: First activation function for intra-opinion activations
-            S2: Second activation functions for inter-opinion activations.
             time_interval: time step for the Euler method applied at every step() call.
             noise_std: Standard deviation of the noise added to the computation of F 
                         upon step() calls.
@@ -39,10 +35,10 @@ class GODAgentNetwork(agent_network.AgentNetwork):
 
         self._adjacency_tensor = params["adjacency_tensor"]
         self._resistance = params["resistance"]
-        self._attention = params["attention"]
+        self._same_option_attention = params["same_option_attention"]
+        self._other_options_attention = params["other_options_attention"]
         self._input = params["inputs"]
-        self._S1 = S1
-        self._S2 = S2
+        self._S = np.tanh
         super().__init__(adjacency_matrix, agents)
         self._n_agents, self._n_options = self.agents.shape
         self._time_interval = time_interval
@@ -54,21 +50,20 @@ class GODAgentNetwork(agent_network.AgentNetwork):
 
     def _step(self, time_interval: float = None) -> None:
         """
-        Updates the General Opinion Dynamics Agent Network. Euler method is used, with possibly added noise at
+        Updates the Agent Network. Euler method is used, with possibly added noise at
         every time step.
         
         If time_interval arg is provided, it will use this instead of the object's self._time_interval attribute.
         """
         t = time_interval or self._time_interval
-        F = (
-            (-self._resistance * self._agents
-             + self._attention * (self._S1(np.einsum('ikj,kj->ij', np.einsum('...ii->...i', self._adjacency_tensor),
-                                                     self._agents))
-                                  + np.sum(self._S2(np.einsum('ikjl,kl->ijl', self._adjacency_tensor, self._agents)),
-                                           axis=2, where=self._non_diag_bool_tensor))
-             + self._input
-            ) * t
-            + np.random.normal(size=(self._n_agents, self._n_options), scale=self._noise_std) * (t**0.5))
+        F = ((-self._resistance * self._agents
+              + self._same_option_attention * self._S(np.einsum('ikj,kj->ij', np.einsum('...ii->...i', self._adjacency_tensor),
+                                                                self._agents))
+              + self._other_options_attention * np.sum(self._S(np.einsum('ikjl,kl->ijl', self._adjacency_tensor, self._agents)),
+                                                       axis=2, where=self._non_diag_bool_tensor)
+              + self._input
+              ) * t
+             + np.random.normal(size=(self._n_agents, self._n_options), scale=self._noise_std) * (t**0.5))
 
         delta_z = F - np.mean(F, axis=1, keepdims=True)
 
