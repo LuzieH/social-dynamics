5418904f0195a65bbf039c9b5eddfaaefc99df31
FMalerba <malerbafede@gmail.com>
2021-03-21 16:47:31 +0100

    Changed Metric abstract class to also support saving of the metrics from within the metric classes

--- social_dynamics/metrics/metric.py
+++ social_dynamics/metrics/metric.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Dict, Union
 import numpy as np
 
 
@@ -9,23 +9,31 @@ class Metric(ABC):
         self._name = name
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
+    
+    def __call__(self, *args, **kwargs) -> None:
+        return self.call(*args, **kwargs)
 
     @abstractmethod
     def call(self, *args, **kwargs) -> None:
         """Accumulates statistics for the metric. Users should use __call__ instead."""
-        pass
 
     @abstractmethod
     def reset(self) -> None:
         """Resets the values being tracked by the metric."""
-        pass
 
     @abstractmethod
-    def result(self) -> Union[np.ndarray, float]:
+    def result(self) -> Union[Dict[str, np.ndarray], float, np.ndarray]:
         """Computes and returns a final value for the metric."""
-        pass
 
-    def __call__(self, *args, **kwargs) -> None:
-        return self.call(*args, **kwargs)
+    @abstractmethod
+    def save(self, save_path: str, time_step: int) -> None:
+        """Saves the cumulated results of the metric
+
+        Args:
+            save_path (str): Path to the experiment_dir where to create the metric's save
+                        folder and save its results
+            time_step (int): time step associated with current save call
+        """
+    
