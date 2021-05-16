4447a9afb00babadf3a11a666a0162a4da47aafd
FMalerba <malerbafede@gmail.com>
2021-05-16 11:34:09 +0200

    CNN check can now be run in a loop and added embedding size calculator.

--- autoencoder_clustering/utils.py
+++ autoencoder_clustering/utils.py
@@ -11,15 +11,22 @@ from tensorflow.keras.optimizers import Adam
 from typing import Callable, Dict, List, Tuple
 
 
-def check_cnn_autoencoder_shapes(time_series_length: int, layers_kwargs: List[Dict[str, int]]) -> None:
+def check_cnn_autoencoder_shapes(time_series_length: int, layers_kwargs: List[Dict[str, int]]) -> bool:
     current_len = time_series_length
-    print(f"\n\nCNN layers' lengths:\n{current_len}")
-    for i, layer_kwargs in enumerate(layers_kwargs):
+    for layer_kwargs in layers_kwargs:
         current_len = (current_len - layer_kwargs["kernel_size"]) / layer_kwargs["strides"] + 1
-        print(current_len)
         if current_len % 1:
-            raise RuntimeError("Convolutional layers' kwargs are not usable for a CNN autoencoder. "
-                               f"First problem arises at layer {i} (counting from 0)")
+            return False
+    
+    return True
+
+
+def compute_embedding_size(time_series_length: int, layers_kwargs: List[Dict[str, int]]) -> bool:
+    current_len = time_series_length
+    for layer_kwargs in layers_kwargs:
+        current_len = (current_len - layer_kwargs["kernel_size"]) / layer_kwargs["strides"] + 1
+    
+    return current_len * layers_kwargs[-1]["filters"]
 
 
 def load_numpy_file(file_path: str, downsampling: int) -> np.ndarray:
