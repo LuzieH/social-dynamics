71aecfd49c3f65430719a01bcb1f82c86dc88e9a
FMalerba <malerbafede@gmail.com>
2021-06-17 23:55:04 +0200

    Major cleanup of the code.

--- social_dynamics/autoencoder_utils.py
+++ social_dynamics/autoencoder_utils.py
@@ -8,29 +8,23 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Conv1D, Conv1DTranspose, Dense, Dropout, SpatialDropout1D
 from tensorflow.keras.optimizers import Adam
 
-from typing import Callable, Dict, List, Tuple
+from typing import Dict, List, Tuple
 
 
-def check_cnn_autoencoder_shapes(time_series_length: int, layers_kwargs: List[Dict[str, int]]) -> bool:
+def compute_embedding_length(time_series_length: int, layers_kwargs: List[Dict[str, int]]) -> int:
     current_len = time_series_length
     for layer_kwargs in layers_kwargs:
         current_len = (current_len - layer_kwargs["kernel_size"]) / layer_kwargs["strides"] + 1
         if current_len % 1:
-            return False
+            return -1
     
-    return True
+    return current_len
 
 
-def compute_embedding_size(time_series_length: int, layers_kwargs: List[Dict[str, int]]) -> bool:
-    current_len = time_series_length
-    for layer_kwargs in layers_kwargs:
-        current_len = (current_len - layer_kwargs["kernel_size"]) / layer_kwargs["strides"] + 1
-    
-    return current_len * layers_kwargs[-1]["filters"]
-
-
-def load_numpy_file(file_path: str, downsampling: int) -> np.ndarray:
+def load_numpy_file(file_path: str, downsampling: int, last_portion: bool) -> np.ndarray:
     data = np.load(file_path.numpy().decode())[::downsampling]
+    if last_portion:
+        data = data[int(data.shape[0]*0.75):]
     return data.astype(np.float32)
 
 
@@ -39,42 +33,49 @@ def dnn_data_preprocessing(exp_data: tf.Tensor) -> tf.Tensor:
     return tensor, tensor
 
 
-def dnn_data_pipeline(file_path: str, shape: tf.TensorShape, load_func: Callable[[str, int],
-                                                                                 np.ndarray]) -> tf.Tensor:
-    [exp_data,] = tf.py_function(load_func, [file_path], [tf.float32,])
-    exp_data.set_shape(shape)
-    inputs, outputs = dnn_data_preprocessing(exp_data)
-    return inputs, outputs
-
-
 def cnn_data_preprocessing(exp_data: tf.Tensor) -> tf.Tensor:
     tensor = tf.reshape(exp_data, [tf.shape(exp_data)[0], -1])
     return tensor, tensor
 
 
-def cnn_data_pipeline(file_path: str, shape: tf.TensorShape, load_func: Callable[[str, int],
-                                                                                 np.ndarray]) -> tf.Tensor:
-    [exp_data,] = tf.py_function(load_func, [file_path], [tf.float32,])
-    exp_data.set_shape(shape)
-    inputs, outputs = cnn_data_preprocessing(exp_data)
-    return inputs, outputs
+def create_dataset(series_dir: Path, downsampling: int, model_type: str, last_portion: bool) -> tf.data.Dataset:
+    """Creates a tensorflow.data.Dataset object to be used for training of the autoencoder.
 
+    Args:
+        series_dir (Path): Directory where the results for all experiments in the series can be found.
+                    These will be used as samples to train the autoencoder.
+        downsampling (int): Downsampling factor applied to the results.
+        model_type (str): String in ["cnn", "dnn"]. This is necessary since the two kinds of models require
+                    different input shapes.
+        last_portion (bool): Boolean for whether to feed the last 25% of an experiment as input instead of
+                    the entire run.
 
-def create_dataset(series_dir: Path, downsampling: int, model_type: str) -> tf.data.Dataset:
+    Raises:
+        ValueError: If model_type is not supported.
+
+    Returns:
+        tf.data.Dataset: Dataset to be used for training the autoencoder.
+    """
     if model_type not in ["cnn", "dnn"]:
         raise ValueError(f"Invalid model_type ({model_type})argument passed to the function.")
     example_file = next(series_dir.iterdir()).joinpath("StateMetric", "results_t200000.npy")
-    shape = tf.TensorShape(np.load(example_file)[::downsampling].shape)
-    data_pipeline_func = partial(cnn_data_pipeline if model_type == "cnn" else dnn_data_pipeline,
-                                 shape=shape,
-                                 load_func=partial(load_numpy_file, downsampling=downsampling))
+    load_func = partial(load_numpy_file, downsampling=downsampling, last_portion=last_portion)
+    shape = tf.TensorShape(load_func(example_file).shape)
+    
+    preprocessing_func = dnn_data_preprocessing if model_type == "dnn" else cnn_data_preprocessing
+    def data_pipeline(file_path: str) -> tf.Tensor:
+        [exp_data,] = tf.py_function(load_func, [file_path], [tf.float32,])
+        exp_data.set_shape(shape)
+        inputs, outputs = preprocessing_func(exp_data)
+        return inputs, outputs
+    
     file_pattern = str(series_dir) + "/*/StateMetric/results_t200000.npy"
     dataset = tf.data.Dataset.list_files(file_pattern=file_pattern, shuffle=False)
-    dataset = dataset.map(data_pipeline_func, num_parallel_calls=tf.data.experimental.AUTOTUNE).cache()
+    dataset = dataset.map(data_pipeline, num_parallel_calls=tf.data.experimental.AUTOTUNE).cache()
     return dataset
 
 
-def get_dnn_autoencoder_model(input_shape: int, layer_sizes: Tuple[int], dropout_rate: float, sigmoid=False):
+def get_dnn_autoencoder_model(input_shape: int, layer_sizes: Tuple[int], dropout_rate: float, sigmoid=False) -> Model:
     model_input = model = Input(shape=input_shape)
 
     for layer_size in layer_sizes[:-1]:
@@ -101,7 +102,7 @@ def get_dnn_autoencoder_model(input_shape: int, layer_sizes: Tuple[int], dropout
 def get_cnn_autoencoder_model(input_shape: Tuple[int],
                               layers_kwargs: List[Dict[str, int]],
                               dropout_rate: float,
-                              sigmoid=False):
+                              sigmoid=False) -> Model:
     model_input = model = Input(shape=input_shape)
 
     for layer_kwargs in layers_kwargs[:-1]:
