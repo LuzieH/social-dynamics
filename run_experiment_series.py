e32a3bd5a43c0c46796113f23dc1f3f39a12a7c8
FMalerba <malerbafede@gmail.com>
2021-06-18 13:34:40 +0200

    Reformatting and cleanup of the code. Batch size is now an optional command line parameter.

--- run_experiment_series.py
+++ run_experiment_series.py
@@ -16,11 +16,10 @@ from social_dynamics.agent_networks.agent_network import AgentNetwork
 from social_dynamics.agent_networks.god_agent_network.god_agent_network import GODAgentNetwork
 from social_dynamics.agent_networks.luzie_agent_network.luzie_agent_network import LuzieAgentNetwork
 
-
 LOCKS_PATH = Path("locks")
 
 
-def generate_experiment_params_batch(series_dir: Path) -> Union[None, np.ndarray]:
+def generate_experiment_params_batch(series_dir: Path, batch_size: int) -> Union[None, np.ndarray]:
     """Returns a batch of yet to-be-done experiments.
     
     Note that this does not avoid multiple processes trying to work on the same experiment at the same time.
@@ -37,28 +36,28 @@ def generate_experiment_params_batch(series_dir: Path) -> Union[None, np.ndarray
         Union[None, np.ndarray]: None if there are no experiments left to run, list of experiment params
                     otherwise
     """
-    batch_size = 20
     experiment_params_array = np.array(list(product(np.linspace(-2, 2, 11), repeat=4)))
-    experiment_path_list = [series_dir.joinpath(generate_experiment_name(alpha=alpha, beta=beta,
-                                                                         gamma=gamma, delta=delta)
-                                                )
-                            for alpha, beta, gamma, delta in experiment_params_array]
-    
+    experiment_path_list = [
+        series_dir.joinpath(generate_experiment_name(alpha=alpha, beta=beta, gamma=gamma, delta=delta))
+        for alpha, beta, gamma, delta in experiment_params_array
+    ]
+
     completed_experiments = np.array(list(series_dir.iterdir()))
-    
-    to_do_experiments = experiment_params_array[~np.isin(experiment_path_list, completed_experiments)].tolist()
-    
+
+    to_do_experiments = experiment_params_array[~np.isin(experiment_path_list, completed_experiments)].tolist(
+    )
+
     # The list gets shuffled to decrease the likelihood that multiple processes spawned at the same time
     # might collide on the same locks.
     rng = np.random.default_rng()
     rng.shuffle(to_do_experiments)
-    
+
     return to_do_experiments[:batch_size]
 
 
-def generate_experiment_name(alpha:float, beta: float, gamma: float, delta:float) -> str:
-    return "{}alpha_{}beta_{}gamma_{}delta".format(np.round(alpha, 1), np.round(beta, 1),
-                                                   np.round(gamma, 1), np.round(delta, 1))
+def generate_experiment_name(alpha: float, beta: float, gamma: float, delta: float) -> str:
+    return "{}alpha_{}beta_{}gamma_{}delta".format(np.round(alpha, 1), np.round(beta, 1), np.round(gamma, 1),
+                                                   np.round(delta, 1))
 
 
 def check_lock(results_path: Path) -> bool:
@@ -69,7 +68,7 @@ def check_lock(results_path: Path) -> bool:
     Returns:
         bool: Whether to execute the run or not.
     """
-    time.sleep(np.random.uniform(high=10))      # Decreases the likelihood of colliding processes on same lock
+    time.sleep(np.random.uniform(high=10))  # Decreases the likelihood of colliding processes on same lock
     lock_name = results_path.name + ".npy"
     lock_path = LOCKS_PATH.joinpath(lock_name)
     if results_path.exists() or lock_path.exists():
@@ -99,12 +98,8 @@ def release_lock(results_path: Path) -> None:
 
 
 @gin.configurable
-def run_experiment(series_dir: Path,
-                   experiment_name: str,
-                   num_time_steps: int,
-                   agent_network: AgentNetwork,
-                   checkpoint_interval: int,
-                   metrics_interval: int) -> None:
+def run_experiment(series_dir: Path, experiment_name: str, num_time_steps: int, agent_network: AgentNetwork,
+                   checkpoint_interval: int, metrics_interval: int) -> None:
     """Runs a single experiment in the series. Saves the results of the metrics 
 
     Args:
@@ -123,10 +118,10 @@ def run_experiment(series_dir: Path,
     experiment_dir = series_dir.joinpath(experiment_name)
     if not experiment_dir.is_dir():
         experiment_dir.mkdir(parents=True)
-    
+
     metrics = utility.setup_metrics(checkpoint_interval=checkpoint_interval,
                                     metrics_interval=metrics_interval)
-    
+
     for t in range(1, num_time_steps + 1):
 
         agent_network.step()
@@ -139,7 +134,7 @@ def run_experiment(series_dir: Path,
             for metric in metrics:
                 metric.save(save_path=experiment_dir, time_step=t)
                 metric.reset()
-    
+
     for metric_dir in experiment_dir.iterdir():
         files = sorted(metric_dir.iterdir(), key=os.path.getmtime)
         files = [file for file in files]
@@ -151,14 +146,17 @@ def run_experiment(series_dir: Path,
 
 def run_experiment_series(root_dir: Path,
                           series_name: str,
+                          batch_size: int,
                           random_seed: Optional[int] = None,
                           random_state_path: Optional[str] = None) -> None:
     """Runs a series of experiments iterating through certain parameters as determined by the customizable
     for loops below.
 
     Args:
-        root_dir (str): The directory where results for all experiments are saved 
+        root_dir (Path): The directory where results for all experiments are saved 
                 (relative path based on where the terminal is located)
+        series_name (str): Name with which to refer to this series and to name its directory of results.
+        batch_size (int): Batch size to be use by the loop over experiments.
         random_seed (Optional[int], optional): Random seed to be used for replicability. Defaults to None.
         random_state_path (Optional[str], optional): Path to the random state to be loaded and used as seed for replicability.
                 Defaults to None.
@@ -181,47 +179,51 @@ def run_experiment_series(root_dir: Path,
     else:
         np.random.seed(random_seed)
     random_state = np.random.get_state()
-    np.save(series_dir.joinpath('initial_random_state.npy'),
-            np.array(random_state, dtype='object'))
-    
+    np.save(series_dir.joinpath('initial_random_state.npy'), np.array(random_state, dtype='object'))
+
     # The reason for sampling batches multiple times instead of just iterating through everything is that
     # this way we regularly clearup any experiments already done. This saves on a lot of I/O operations
     # to the disk, particularly towards the end when there can be thousands of folders already done.
-    experiment_params_batch = generate_experiment_params_batch(series_dir=series_dir)
+    experiment_params_batch = generate_experiment_params_batch(series_dir=series_dir, batch_size=batch_size)
     while experiment_params_batch:
         for alpha, beta, gamma, delta in tqdm(experiment_params_batch):
-            agent_network = LuzieAgentNetwork(builders_kwargs={"adj_matrix_builder_kwargs": dict(),
-                                                               "agents_builder_kwargs": dict(),
-                                                               "parameters_builder_kwargs": {"alpha": alpha,
-                                                                                             "beta": beta,
-                                                                                             "gamma": gamma,
-                                                                                             "delta": delta}
-                                                               })
-            experiment_name = generate_experiment_name(alpha=alpha, beta=beta,
-                                                       gamma=gamma, delta=delta)
-            
+            agent_network = LuzieAgentNetwork(
+                builders_kwargs={
+                    "adj_matrix_builder_kwargs": dict(),
+                    "agents_builder_kwargs": dict(),
+                    "parameters_builder_kwargs": {
+                        "alpha": alpha,
+                        "beta": beta,
+                        "gamma": gamma,
+                        "delta": delta
+                    }
+                })
+            experiment_name = generate_experiment_name(alpha=alpha, beta=beta, gamma=gamma, delta=delta)
+
             results_path = series_dir.joinpath(experiment_name)
-            
-            if not check_lock(results_path): continue
-            
+
+            if not check_lock(results_path):
+                continue
+
             acquire_lock(results_path)
-            
+
             run_experiment(series_dir=series_dir,
                            experiment_name=experiment_name,
                            agent_network=agent_network,
                            metrics_interval=50)
-            
-            release_lock(results_path)
-        
-        experiment_params_batch = generate_experiment_params_batch(series_dir=series_dir)
 
+            release_lock(results_path)
+
+        experiment_params_batch = generate_experiment_params_batch(series_dir=series_dir)
 
 
 def main(_) -> None:
     logging.set_verbosity(logging.INFO)
     utility.load_gin_configs(FLAGS.gin_files, FLAGS.gin_bindings)
-    
-    run_experiment_series(root_dir=Path(FLAGS.root_dir), series_name=FLAGS.series_name)
+
+    run_experiment_series(root_dir=Path(FLAGS.root_dir),
+                          series_name=FLAGS.series_name,
+                          batch_size=FLAGS.batch_size)
 
 
 if __name__ == '__main__':
@@ -235,7 +237,11 @@ if __name__ == '__main__':
     flags.DEFINE_multi_string(
         'gin_bindings', [], 'Gin bindings to override the values set in the config files '
         '(e.g. "run_experiment.random_seed=42").')
+    flags.DEFINE_integer('batch_size', 10, 'Batch size for the experiment loop.')
+    flags
     FLAGS = flags.FLAGS
     flags.mark_flag_as_required('root_dir')
+    flags.mark_flag_as_required('series_name')
+    flags.mark_flag_as_required('gin_files')
 
     app.run(main)
