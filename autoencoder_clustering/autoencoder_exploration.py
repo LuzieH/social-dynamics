71e279297fecff1788512decc0856f692b773d67
FMalerba <malerbafede@gmail.com>
2021-07-24 22:57:52 +0200

    Cleanup of code and reformatting.

--- autoencoder_clustering/autoencoder_exploration.py
+++ autoencoder_clustering/autoencoder_exploration.py
@@ -2,6 +2,7 @@ from absl import app
 from absl import flags
 from absl import logging
 
+from functools import partial
 import numpy as np
 import os
 from pathlib import Path
@@ -14,10 +15,7 @@ from social_dynamics.autoencoder_utils import get_dnn_autoencoder_model, get_cnn
 import tensorflow as tf
 from tensorflow.keras.losses import MSE
 
-
-LOGGING_DICT = {"debug": logging.DEBUG,
-                "info": logging.INFO,
-                "warning": logging.WARNING}
+LOGGING_DICT = {"debug": logging.DEBUG, "info": logging.INFO, "warning": logging.WARNING}
 downsampling = 4
 
 
@@ -29,16 +27,14 @@ def run_autoencoder_exploration(root_dir: str, series_dir: str, batch_size: int)
     root_dir_path = Path(root_dir)
     results_dir_path = root_dir_path.joinpath("autoencoders_results")
     series_dir_path = Path(series_dir)
-    
-    
+
     datasets = load_all_datasets(series_dir=series_dir_path, downsampling=downsampling)
-    
+
     input_shapes = determine_input_shapes(datasets)
-    
+
     with open(root_dir_path.joinpath('models_kwargs.pickle'), 'rb') as handle:
         models_kwargs = pickle.load(handle)
-    
-    
+
     keys = list(models_kwargs.keys())
     rng = np.random.default_rng()
     rng.shuffle(keys)
@@ -47,36 +43,36 @@ def run_autoencoder_exploration(root_dir: str, series_dir: str, batch_size: int)
         model_type, input_type = key.split("-")
         experiment_params_list = [{"key": key, "model_index": model_index}
                                   for model_index in range(len(models_kwargs[key]))]
-        
-        
+
         dataset = datasets[key].shuffle(buffer_size=20_000).batch(128).prefetch(tf.data.experimental.AUTOTUNE)
         y_true = np.array(list(datasets[key].as_numpy_iterator()))[:, 1]
         input_shape = input_shapes[key]
-        
-        
-        experiment_params_batch = utility.generate_experiment_params_batch(
-            all_results_dir=results_dir_path,
-            experiment_params_list=experiment_params_list,
-            experiment_name_generator=generate_experiment_name,
-            batch_size=batch_size)
+
+        batch_generator = partial(utility.generate_experiment_params_batch,
+                                  all_results_dir=results_dir_path,
+                                  experiment_params_list=experiment_params_list,
+                                  experiment_name_generator=generate_experiment_name,
+                                  batch_size=batch_size)
+        experiment_params_batch = batch_generator()
         while experiment_params_batch:
             for experiment_params in experiment_params_batch:
                 model_index = experiment_params["model_index"]
                 experiment_name = generate_experiment_name(key, model_index)
-                
+
                 model_results_path = results_dir_path.joinpath(experiment_name)
-                if not utility.check_lock(model_results_path): continue
-                
+                if not utility.check_lock(model_results_path):
+                    continue
+
                 utility.acquire_lock(model_results_path)
-                
+
                 logging.info("Acquired lock on {}".format(experiment_name))
-                
+
                 model_kwargs = models_kwargs[key][model_index]
                 if model_type == "cnn":
                     model = get_cnn_autoencoder_model(input_shape, **model_kwargs, sigmoid=False)
                 else:
                     model = get_dnn_autoencoder_model(input_shape, **model_kwargs, sigmoid=False)
-                
+
                 model_hist = model.fit(dataset, epochs=30, verbose=0)
                 y_preds = model.predict(y_true)
                 # CNNs have bidimensional output (time x agent-option_channel) and thus MSEb needs
@@ -85,32 +81,28 @@ def run_autoencoder_exploration(root_dir: str, series_dir: str, batch_size: int)
                     mses = np.mean(MSE(y_true, y_preds).numpy(), axis=-1)
                 else:
                     mses = MSE(y_true, y_preds).numpy()
-                
-                
+
                 model_results_path.mkdir(parents=True, exist_ok=True)
                 np.save(model_results_path.joinpath("history.npy"), model_hist.history)
                 np.save(model_results_path.joinpath("mses.npy"), mses)
-                
+
                 tf.keras.backend.clear_session()
-                
+
                 utility.release_lock(model_results_path)
                 logging.info("Saved and released lock on {}".format(experiment_name))
-            
-            
-            experiment_params_batch = utility.generate_experiment_params_batch(
-                all_results_dir=results_dir_path,
-                experiment_params_list=experiment_params_list,
-                experiment_name_generator=generate_experiment_name,
-                batch_size=batch_size)
+
+            experiment_params_batch = batch_generator()
 
 
 def main(_) -> None:
     logging.set_verbosity(LOGGING_DICT[FLAGS.logging])
-    
+
     gpus = tf.config.experimental.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(gpus[0], True)
-    
-    run_autoencoder_exploration(root_dir=FLAGS.root_dir, series_dir=FLAGS.series_dir, batch_size=FLAGS.batch_size)
+
+    run_autoencoder_exploration(root_dir=FLAGS.root_dir,
+                                series_dir=FLAGS.series_dir,
+                                batch_size=FLAGS.batch_size)
 
 
 if __name__ == '__main__':
@@ -124,6 +116,5 @@ if __name__ == '__main__':
     FLAGS = flags.FLAGS
     flags.mark_flag_as_required('root_dir')
     flags.mark_flag_as_required('series_dir')
-    
-    app.run(main)
 
+    app.run(main)
