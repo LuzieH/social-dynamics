405f2e74c8ba0991ac0e306038edb3e94084c12a
FMalerba <malerbafede@gmail.com>
2021-08-27 10:38:31 +0200

    Fix.

--- slurm_job_scripts/model_list_generator_job.sh
+++ slurm_job_scripts/model_list_generator_job.sh
@@ -25,6 +25,6 @@ export LD_LIBRARY_PATH=/usr/local/cuda-11/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_
 ## Which for example logs when, if, why tasks have completed/stopped. This output location is instead for the
 ## single task (therefore there are actually ntasks output files being generated here).
 srun --export=ALL --ntasks=1 -o /home/htc/fmalerba/logs/%x_%J_%t.out python code/social-dynamics/autoencoder_clustering/model_list_generator.py \
---root_dir="/home/htc/fmalerba/autoencoder_clustering" \
+--root_dir="/scratch/htc/fmalerba/autoencoder_clustering" \
 --series_dir="/home/htc/fmalerba/experiments_results/2_opt-h_luzie-alpha_beta_gamma_delta_expl-0.0001t"
 wait
